349f6b291ef3e1badca5ac7665ab42c153e7861a
josherz94 <josherz94@gmail.com>
2020-04-21 09:25:50 -0700

    updated game
    FIXED:
    -enemy controller
    -char controller bounds
    -background wasn't scrolling after pushed to repo
    -diversified enemys
    -asteroids now rotate
    -projectiles conflicting with ui
    -added sounds/music
    
    BUGS/FIXES NEEDED:
    -explosion sound effect not working/buggy. possible fix to just get another sound file
    -player hp not effected by enemy projectiles
    -game doesn't seem to end when hp reaches 0
    -2 character controller scripts?
    -updated code for enemy is in the script titled "enemy"

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // only need to randomize between y axis values
+    public float min_Y = -4.3f, max_Y = 4.3f;
+
+    public GameObject[] asteroids;
+    public GameObject[] enemies;
+
+    public float timer = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Spawn", timer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Spawn()
+    {
+        float pos_Y = Random.Range(min_Y, max_Y);
+        Vector2 temp = transform.position;
+        temp.y = pos_Y;
+
+        
+        if(Random.Range(0,2) > 0)
+        {
+            Instantiate(asteroids[Random.Range(0, asteroids.Length)], temp, Quaternion.identity);
+        } else {
+            Instantiate(enemies[Random.Range(0, enemies.Length)], temp, Quaternion.identity);
+        }
+            
+        
+      
+        Invoke("Spawn", timer);
+    }
+}
---
+++ Assets/Script/bulletScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float speed = 30f;
+    public float deactivate_Timer = 3f;
+    public int damage = 1;
+
+    [HideInInspector]
+    public bool is_EnemyBullet = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (is_EnemyBullet)
+            speed *= -1; //set to negative value to go left
+        Invoke("DeactivateGameObject", deactivate_Timer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+
+    void DeactivateGameObject()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+            Destroy(this);
+        }
+    }
+}
